0328c8526c4a830e43eba8e629a8abfdde20b019
Mitsuru Hayasaka <hayasaka.mitsuru@gmail.com>
2012-01-25 14:56:33 +0900

    some statuses

--- maker.js
+++ maker.js
@@ -15,8 +15,43 @@
 			jQuery(".cost").text(jQuery(this).val());
 		});
 
+		jQuery("#level").change(function() {
+			jQuery(".level").text(jQuery(this).val());
+		});
+
+		jQuery("#att").change(function() {
+			jQuery(".status_att").text(jQuery(this).val());
+		});
+		jQuery("#int").change(function() {
+			jQuery(".status_int").text(jQuery(this).val());
+		});
+		jQuery("#sdef").change(function() {
+			jQuery(".status_sdef").text(jQuery(this).val());
+		});
+		jQuery("#bdef").change(function() {
+			jQuery(".status_bdef").text(jQuery(this).val());
+		});
+		jQuery("#wdef").change(function() {
+			jQuery(".status_wdef").text(jQuery(this).val());
+		});
+		jQuery("#rdef").change(function() {
+			jQuery(".status_rdef").text(jQuery(this).val());
+		});
+		jQuery("#speed").change(function() {
+			jQuery(".status_speed").text(jQuery(this).val());
+		});
+
+
 		jQuery("#rarerity").change();
 		jQuery("#name").change();
 		jQuery("#cost").change();
+		jQuery("#level").change();
+		jQuery("#att").change();
+		jQuery("#int").change();
+		jQuery("#sdef").change();
+		jQuery("#bdef").change();
+		jQuery("#wdef").change();
+		jQuery("#rdef").change();
+		jQuery("#speed").change();
 	});
 })();
